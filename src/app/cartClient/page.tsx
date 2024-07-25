75da50fec0116da8c2c6b0530660b8157d79689a
MUHAMMAD <umarasqaraliov@gmail.com>
2024-07-25 23:15:48 +0500

    change commit

--- src/app/cartClient/page.tsx
+++ src/app/cartClient/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-interface CartClientMessage {
+ export interface CartClientMessage {
     message: string;
 }
 
